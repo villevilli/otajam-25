7d2947b98ded6d3d6b71aede252debd7bfecd1a7
ABrokenWAVE <88666518+ABrokenWAVE@users.noreply.github.com>
2025-01-25 00:19:49 +0200

    added flipping sprite and removed normalisation

--- characters/player/player-movement.gd
+++ characters/player/player-movement.gd
@@ -1,9 +1,17 @@
 extends CharacterBody2D
 
 @export var speed: float = 400
+@export var sprite: Sprite2D = null
 
-func _physics_process(delta:float)->void:
-	var input_direction:Vector2 = Input.get_vector("left", "right", "up", "down")
+func _physics_process(_delta:float)->void:
+	#var input_direction:Vector2 = Input.get_vector("left", "right", "up", "down")
+	var input_x:float = Input.get_axis("left","right")
+	var input_y:float =Input.get_axis("up","down")
+	var input_direction: Vector2 = Vector2(input_x,input_y)
 	velocity = input_direction * speed
-	print(position)
+	
+	if input_x<0:
+		sprite.flip_h=true
+	if input_x>0:
+		sprite.flip_h=false
 	move_and_slide()
