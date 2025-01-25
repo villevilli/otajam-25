47ba75f383553c9fda85a6e1970b3926f1aa6eab
ABrokenWAVE <88666518+ABrokenWAVE@users.noreply.github.com>
2025-01-25 18:07:26 +0200

    attack head

--- characters/player/player-movement.gd
+++ characters/player/player-movement.gd
@@ -3,9 +3,19 @@ class_name Player
 
 @export var speed: float = 500
 @onready var sprite: AnimatedSprite2D = $"Sprite"
+@onready var head_atck: Sprite2D = $"HeadAttack"
+@onready var head_cute: Sprite2D = $"HeadCute"
 
 func _ready() -> void:
 	sprite.play()
+func _process(delta: float) -> void:
+	var attacking: bool = Input.is_action_pressed("attack")
+	if attacking:
+		head_atck.show()
+		head_cute.hide()
+	else:
+		head_atck.hide()
+		head_cute.show()
 
 func _physics_process(_delta: float) -> void:
 	#var input_direction:Vector2 = Input.get_vector("left", "right", "up", "down")
@@ -16,6 +26,10 @@ func _physics_process(_delta: float) -> void:
 	
 	if input_x < 0:
 		sprite.flip_h = false
+		head_atck.flip_h = false
+		head_cute.flip_h = false
 	if input_x > 0:
 		sprite.flip_h = true
+		head_atck.flip_h = true
+		head_cute.flip_h = true
 	move_and_slide()
