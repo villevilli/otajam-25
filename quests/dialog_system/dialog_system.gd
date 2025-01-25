3a6901badce38107d97df0f5c8f43e02d1b83063
Ville Kujala <kujala.ville.vk@gmail.com>
2025-01-25 07:42:39 +0200

    adds typewriter effect to dialog

--- quests/dialog_system/dialog_system.gd
+++ quests/dialog_system/dialog_system.gd
@@ -1,25 +1,33 @@
 extends Control
 class_name DialogSystem
 
-@export var current_dialog: DialogNode:
+@export var dialog_letter_delay_ms: int = 10
+
+var current_dialog: DialogNode:
 	set(dialog_node):
 		current_dialog = dialog_node
-		match typeof(dialog_node):
-			StraightDialog:
-				pass
-			EndDialog:
-				pass
+		_reset_simple_text()
 
-@onready var simple_dialog_text: RichTextLabel = get_node("DialogPanel/SimpleDialog/HBoxContainer/ScrollContainer/MarginContainer/SimpleDialogText")
-@onready var dialog_panel: Panel = get_node("%DialogPanel")
+@export var start_dialog: DialogNode
 
+@onready var simple_dialog_text: RichTextLabel = $"%SimpleDialogText"
+@onready var dialog_panel: Panel = $"%DialogPanel"
+
+var ms_elapsed: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	current_dialog = start_dialog
 	dialog_panel.connect("gui_input", _dialog_input)
 	pass # Replace with function body.
 
 func _process(_delta: float) -> void:
+	 
+	ms_elapsed += int(_delta * 1000)
+
+	if ms_elapsed >= dialog_letter_delay_ms:
+		_reveal_new_letter()
+
 	if current_dialog is StraightDialog:
 			simple_dialog_text.text = (current_dialog as StraightDialog).get_dialog_text()
 	elif current_dialog is EndDialog:
@@ -31,6 +39,18 @@ func _dialog_input(event: InputEvent) -> void:
 		if (event as InputEventMouseButton).button_index == MOUSE_BUTTON_LEFT && (event as InputEventMouseButton).is_released():
 			dialog_clicked()
 
+func _reset_simple_text() -> void:
+	if simple_dialog_text == null:
+		print("null")
+		return
+
+	if simple_dialog_text.visible_ratio >= 1:
+		return
+	
+	simple_dialog_text.visible_characters = 0
+
+func _reveal_new_letter() -> void:
+	simple_dialog_text.visible_characters += 1
 
 func dialog_clicked() -> void:
 	if current_dialog is StraightDialog:
--- quests/dialog_system/straight_dialog.gd
+++ quests/dialog_system/straight_dialog.gd
@@ -4,6 +4,5 @@ class_name StraightDialog
 @export_multiline var dialog_text: String
 @export var next_dialog: DialogNode
 
-
 func get_dialog_text() -> String:
     return dialog_text
