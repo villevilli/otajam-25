de49f31bc78f71d2e2c15bec720892c23dad1b3c
Ville Kujala <kujala.ville.vk@gmail.com>
2025-01-25 20:43:18 +0200

    save loading

--- user_interface/main_menu/main_menu.gd
+++ user_interface/main_menu/main_menu.gd
@@ -1,7 +1,25 @@
 extends Control
 
+var file_dialog: FileDialog
+
 func _on_new_game_pressed() -> void:
-	LevelManager.levelchange.emit("example_level","default")
+	LevelManager.levelchange.emit("example_level", "default")
 
 func _on_close_game_button_down() -> void:
 	get_tree().quit()
+
+
+func _on_load_game_pressed() -> void:
+	file_dialog = FileDialog.new()
+	file_dialog.access = FileDialog.ACCESS_USERDATA
+	file_dialog.popup_exclusive_centered(self, Vector2i(500, 500))
+	file_dialog.current_path = "user://saves/"
+	file_dialog.filters.append("*.json")
+	file_dialog.file_mode = FileDialog.FILE_MODE_OPEN_FILE
+	file_dialog.file_selected.connect(_file_selected)
+
+
+func _file_selected(path: String) -> void:
+	print(path)
+	LevelManager.levelchange.emit("example_level", "default")
+	SaveMager.load_game(path)
--- world/save_mager.gd
+++ world/save_mager.gd
@@ -1,11 +1,15 @@
 extends Node
 
+func _ready() -> void:
+	if !DirAccess.dir_exists_absolute("user://saves/"):
+		DirAccess.make_dir_absolute("user://saves/")
+
 func save_game(file_name: String) -> void:
-	var json := JSON.stringify(QuestManager.serialize())
-	var file := FileAccess.open("user://" + file_name + "/quests.json", FileAccess.WRITE)
+	var json := JSON.stringify({"quests": QuestManager.serialize()})
+	var file := FileAccess.open("user://saves/" + file_name + ".json", FileAccess.WRITE)
 	file.store_string(json)
 
-func load_game(file_name: String) -> void:
-	var file := FileAccess.open("user://" + file_name + "/quests.json", FileAccess.READ)
+func load_game(file_path: String) -> void:
+	var file := FileAccess.open(file_path, FileAccess.READ)
 	var json := file.to_string()
-	QuestManager.deserialize(JSON.parse_string(json))
+	QuestManager.deserialize(JSON.parse_string(json)["quests"])
